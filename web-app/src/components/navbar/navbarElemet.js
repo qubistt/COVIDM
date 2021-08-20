112eea103339a5367261c099c333318f74f3b9ac
Udhay <udhay1co.de@gmail.com>
2021-08-20 11:20:07 +0530

    Navbar files

--- web-app/src/components/navbar/navbarElemet.js
+++ web-app/src/components/navbar/navbarElemet.js
@@ -1,11 +1,31 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import {Nav,  Navbar, NavDropdown } from 'react-bootstrap';
 
-const Navbar = () => {
+
+
+
+const navbar = () => {
     return (
       <>
-       <h1>Navbar working!</h1>
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#deets">More details</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Good stuff
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
       </>
     );
   };
     
-  export default Navbar;
+  export default navbar;
