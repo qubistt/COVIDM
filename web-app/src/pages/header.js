3923e90a039fc6fc5e4140dded4cfe020d4ddb99
Udhay <udhay1co.de@gmail.com>
2021-08-20 19:44:40 +0530

    header_image_workinggg

--- web-app/src/pages/header.js
+++ web-app/src/pages/header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css';
-import head from '.'
+import head from './img_header.png'
 
 
 const header = () => {
     return (
         <>
         <h1>Header working</h1>
+        <img src={head} />
+
 
 
         </>
