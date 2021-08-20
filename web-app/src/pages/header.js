e0ee25c21470ea761dd597d3bdd9294013c0e536
Udhay <udhay1co.de@gmail.com>
2021-08-20 20:55:53 +0530

    working on fonts

--- web-app/src/pages/header.js
+++ web-app/src/pages/header.js
@@ -3,12 +3,17 @@ import './App.css';
 import head from './header_img_covid_photo_pea.png'
 
 
+
+
+
 const header = () => {
     return (
         <>
         <div className="header">
         <img className="img1"  src={head} />
         <h1>Covidm</h1>
+
+
 
         </div>
 
