332a25202203baa7c6be37d6e2aa1d4ea50d8d63
Udhay <udhay1co.de@gmail.com>
2021-08-20 10:34:51 +0530

    testing router modules

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-
+import Navbar from "./components/navbarElemet"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>welcome to Covidm</h1>
-      </header>
-    </div>
+
+    <Router>
+
+      <Navbar />
+
+    </Router>
+     
+
   );
 }
 
