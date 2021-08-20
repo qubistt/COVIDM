2fbbc99fd48232af95a68144980584bd75575c00
Udhay <udhay1co.de@gmail.com>
2021-08-20 10:32:43 +0530

    navabar files.

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
---
+++ web-app/src/components/navbarElemet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+      <>
+       <h1>Navbar working!</h1>
+      </>
+    );
+  };
+    
+  export default Navbar;
