8952168bf6a4c96480822a1810bf71800533d379
Udhay <udhay1co.de@gmail.com>
2021-08-20 10:41:05 +0530

    Navbar files working!

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from "./components/navbarElemet"
+import Navbar from "./components/navbar/navbarElemet"
 function App() {
   return (
 
     <Router>
-
+{/* Using the imported file from the compoenets */}
       <Navbar />
 
     </Router>
