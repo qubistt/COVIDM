d2968ca449696985968b3a780d5690a2b3767eaf
Udhay <udhay1co.de@gmail.com>
2021-08-20 21:12:40 +0530

    fonts working with assests folder

--- web-app/src/pages/header.js
+++ web-app/src/pages/header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import head from './header_img_covid_photo_pea.png'
-
+import fonts from '../assets/fonts.css'
 
 
 
@@ -11,7 +11,7 @@ const header = () => {
         <>
         <div className="header">
         <img className="img1"  src={head} />
-        <h1>Covidm</h1>
+        <h1 className="Main">Covidm</h1>
 
 
 
