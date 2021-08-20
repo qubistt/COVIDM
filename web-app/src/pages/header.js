223df5c3d695980cee476da33db2a48f0a86bfd0
Udhay <udhay1co.de@gmail.com>
2021-08-20 19:27:52 +0530

    header_files

---
+++ web-app/src/pages/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+
+const header = () => {
+    return (
+        <>
+        <h1>Header working</h1>
+
+        </>
+
+    );
+};
+
+export default header;
