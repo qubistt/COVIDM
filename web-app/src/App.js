6beda1cf754184ae87ea4707e68b9ea9034a45df
Udhay <udhay1co.de@gmail.com>
2021-08-20 19:02:56 +0530

    navbar changes

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from "./components/navbar/navbarElemet"
+import Navbar from './components/navbarElemet';
 function App() {
   return (
+    // using router for the navbar links
+    
 
     <Router>
 {/* Using the imported file from the compoenets */}
---
+++ web-app/src/components/navbarElemet.js
@@ -0,0 +1,52 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import {Nav,  Navbar, NavDropdown } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+const navbar = () => {
+    return (
+      <>
+<Router>
+ 
+
+
+
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+<img
+          alt=""
+          src="https://d3n8a8pro7vhmx.cloudfront.net/gratafund/pages/274/attachments/original/1589869845/COVID_LAW_MONITOR_logo_germ.png?1589869845"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />
+
+       
+                <Navbar.Brand href="#home">Covidm</Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav>
+                        <Nav.Link href="#about" >About</Nav.Link>
+                        <Nav.Link eventKey={2} href="#about">
+                        
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+      </Router>
+
+
+      
+
+
+      </>
+    );
+  };
+
+
+    
+  export default navbar;
