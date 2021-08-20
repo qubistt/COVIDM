ffa6d42aa8df8fcf94ef96a7e8152563597b4c89
Udhay <udhay1co.de@gmail.com>
2021-08-20 20:29:18 +0530

    added font-awsome, worked with the main page!

--- web-app/src/pages/header.js
+++ web-app/src/pages/header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import './App.css';
-import head from './img_header.png'
+import head from './header_img_covid_photo_pea.png'
 
 
 const header = () => {
     return (
         <>
-        <h1>Header working</h1>
-        <img src={head} />
+        <div className="header">
+        <img className="img1"  src={head} />
+        <h1>Covidm</h1>
+
+        </div>
 
 
 
