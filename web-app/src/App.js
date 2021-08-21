d1de33398b557198bcd8af84c2d35174ec0c45be
Udhay <udhay1co.de@gmail.com>
2021-08-21 13:01:05 +0530

    firebase

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/navbarElemet';
 import Header from './pages/header'
+import * as firebase from 'firebase'
+
+
+
 function App() {
   return (
+    
     // using router for the navbar links
     
 
