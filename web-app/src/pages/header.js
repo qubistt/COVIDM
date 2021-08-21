1fb5e87516727a05d9e7784e9b00df01984b4621
Udhay <udhay1co.de@gmail.com>
2021-08-21 15:40:30 +0530

    scrool text desing

--- web-app/src/pages/header.js
+++ web-app/src/pages/header.js
@@ -1,25 +1,61 @@
-import React from "react";
-import './App.css';
-import head from './header_img_covid_photo_pea.png'
-import fonts from '../assets/fonts.css'
+// import React from "react";
+// import './App.css';
+// import head from './header_img_covid_photo_pea.png'
+// import fonts from '../assets/fonts.css'
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
 
 
 
 
 const header = () => {
+    const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+    const FadeUp = batch(Fade(), Move(), Sticky());
+  
     return (
         <>
-        <div className="scroll-container">
-        <img className="img1"  src={head} />
-        <div classname="scroll-text">
-        <h1 className="Main">Covidm</h1>
-
+ <ScrollContainer>
+      <ScrollPage page={0}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
+          <span style={{ fontSize: "3em" }}>Welcome to Covidm</span>
+          {/* <p  style={{ fontSize: "3em" }}>Scroll down </p>  */}
+          {/* Need to edit to the the P's and the Color mode */}
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={1}>
+        <Animator animation={ZoomInScrollOut}>
+          <span style={{ fontSize: "3em" }}>I'm FadeUpScrollOut ✨</span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={2}>
+        <Animator animation={FadeUp}>
+          <span style={{ fontSize: "3em" }}>I'm FadeUp ⛅️</span>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={3}>
+        <div>
+          <span style={{ fontSize: "3em" }}>
+            <Animator animation={MoveIn(-1000, 0)}>Hello Guys 👋🏻</Animator>
+            <Animator animation={MoveIn(1000, 0)}>Nice to meet you 🙋🏻‍♀️</Animator>- I'm Seonghyeok -
+            <Animator animation={MoveOut(1000, 0)}>Good bye ✋🏻</Animator>
+            <Animator animation={MoveOut(-1000, 0)}>See you 💛</Animator>
+          </span>
         </div>
-        
+      </ScrollPage>
+      <ScrollPage page={4}>
+        <Animator animation={batch(Fade(), Sticky())}>
+          <span style={{ fontSize: "3em" }}>Done</span>
+          <span style={{ fontSize: "3em" }}>
+            There's FadeAnimation, MoveAnimation, StickyAnimation, ZoomAnimation
+          </span>
+        </Animator>
+      </ScrollPage>
+    </ScrollContainer>
+  
 
 
 
-        </div>
+
+
 
 
 
