834659c1cc23903e0a289f8575d9b1374d401f3b
Udhay <udhay1co.de@gmail.com>
2021-08-22 11:30:38 +0530

    update from git desktop

--- web-app/src/pages/profile.js
+++ web-app/src/pages/profile.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './header'
+import Navbar from '../components/navbarElemet'
 
 
 
 function profile (){
     return(
         <Router>
-            <Header />
+               <Navbar />
 
+            <Header />
+         
         </Router>
     )
 }
